5a793ae16449a4494c8bb0dde398183060ce3767
YooJunNam <yjn7448@gmail.com>
2021-07-13 11:49:48 +0900

    Detail 부분 button 눌렀을 때 각자 위 아래로 conponent화 시키기

---
+++ src/components/DetailBox/detailbox.js
@@ -0,0 +1,123 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const DetailBox = ({ infoname }) => {
+  const [openDetail, setOpenDetail] = useState(false);
+  const [rotateBtn, setRotateBtn] = useState(false);
+
+  return (
+    <ProductInfo>
+      <div>
+        <ProductDetailblock>
+          <FirstDetailblock>{infoname}</FirstDetailblock>
+          <SecondDetailblock>
+            <InfoBtn
+              onClick={() => {
+                setOpenDetail((prevState) => !prevState);
+                setRotateBtn((prevState) => !prevState);
+              }}
+            >
+              {rotateBtn == true ? (
+                <Rotation>
+                  <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
+                </Rotation>
+              ) : (
+                <NonRotation>
+                  <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
+                </NonRotation>
+              )}
+            </InfoBtn>
+          </SecondDetailblock>
+        </ProductDetailblock>
+
+        <div>
+          {openDetail == true ? (
+            <ProductDetail>
+              · Italian fabric <br />· 100% Polyester
+            </ProductDetail>
+          ) : undefined}
+        </div>
+      </div>
+    </ProductInfo>
+  );
+};
+const ProductInfo = styled.div`
+  float: left;
+  text-align: left;
+  margin-top: 10px;
+  font-size: 12px;
+  min-height: 48px;
+  font-family: 'Neue Haas Unica Pro', 'Helvetica', 'Arial', sans-serif;
+  font-weight: 700;
+  line-height: 1.3;
+  margin-bottom: -1px;
+  letter-spacing: 0.02em;
+  text-transform: uppercase;
+  @media (max-width: 1060px) {
+    width: 100%;
+  }
+`;
+
+const ProductDetail = styled.p`
+  margin-top: 20px;
+  font-size: 0.8125rem;
+  font-family: 'Neue Haas Unica Pro', 'Helvetica', 'Arial', sans-serif;
+  font-weight: 400;
+  line-height: 1.4;
+  letter-spacing: 0em;
+`;
+
+const ProductDetailblock = styled.div`
+  display: flex;
+  height: 30px;
+  border-bottom: 1px solid;
+  width: 500px;
+  margin-bottom: 20px;
+  @media (max-width: 1060px) {
+    width: 100%;
+  }
+`;
+
+const FirstDetailblock = styled.div`
+  flex: 1;
+  @media (max-width: 1060px) {
+    width: 100%;
+  }
+`;
+
+const SecondDetailblock = styled.div`
+  flex: 1;
+  text-align: right;
+  justfy-content: right;
+  @media (max-width: 1060px) {
+    width: 100%;
+  }
+`;
+
+const InfoBtn = styled.button`
+  cursor: pointer;
+  background: white;
+  height: 25px;
+  border: none;
+`;
+
+const NonRotation = styled.svg`
+  width: 1.5em;
+  height: 30px;
+  cursor: pointer;
+  justify-content: center;
+  @media (max-width: 1060px) {
+    float: right;
+  }
+`;
+
+const Rotation = styled.svg`
+  width: 20px;
+  height: 30px;
+  margin-bottom: 20px;
+  cursor: pointer;
+  justify-content: center;
+  transform: rotateX(180deg);
+`;
+
+export default DetailBox;
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,17 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Select } from 'antd';
+import { Carousel } from 'react-bootstrap';
+import DetailBox from '../components/DetailBox/detailbox';
 
 function Detail() {
-  const [openBtn, setOpenBtn] = useState(false);
-  const [rotateBtn, setRotateBtn] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
   return (
     <Container>
       <ContentContainer>
         <ContentImage>
-          <img src="https://ourlegacy.centracdn.net/client/dynamic/images/8814_a07367d216-m2201cbte__0320-full.jpg"></img>
+          <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+              <img
+                src="https://ourlegacy.centracdn.net/client/dynamic/images/8814_a07367d216-m2201cbte__0320-full.jpg"
+                alt="First slide"
+                style={{ maxWidth: '100%', height: 'auto' }}
+              ></img>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src="https://ourlegacy.centracdn.net/client/dynamic/images/8814_29470eec48-m2201cbte__0327-full.jpg"
+                alt="Second slide"
+                style={{ maxWidth: '100%', height: 'auto' }}
+              ></img>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src="https://ourlegacy.centracdn.net/client/dynamic/images/8814_bd105996a9-m2201cbte__0335-full.jpg"
+                alt="Third slide"
+                style={{ maxWidth: '100%', height: 'auto' }}
+              ></img>
+            </Carousel.Item>
+          </Carousel>
         </ContentImage>
+
         <ContentDetail>
           <Content>
             <span>CAPE PARKA</span> <br />
@@ -22,7 +50,7 @@ function Detail() {
           <Size>
             <Select
               defaultValue="SIZE"
-              style={{ width: '500px', float: 'left' }}
+              style={{ width: '95%', float: 'left' }}
               bordered={false}
             >
               <Select.Option value="46">46</Select.Option>
@@ -32,152 +60,51 @@ function Detail() {
             </Select>
           </Size>
           <div>
-            <AddCart>
+            <AddCart style={{ marginBottom: '40px' }}>
               <span>ADD TO CART</span>
             </AddCart>
           </div>
-          <ProductInfo>
-            <div>
-              <ProductDetailblock>
-                <FirstDetailblock>DETAILS</FirstDetailblock>
-                <SecondDetailblock>
-                  <InfoBtn
-                    onClick={() => {
-                      setOpenBtn((prevState) => !prevState);
-                      setRotateBtn((prevState) => !prevState);
-                    }}
-                  >
-                    {rotateBtn == true ? (
-                      <Rotation>
-                        <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
-                      </Rotation>
-                    ) : (
-                      <NonRotation>
-                        <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
-                      </NonRotation>
-                    )}
-                  </InfoBtn>
-                </SecondDetailblock>
-              </ProductDetailblock>
-
-              <div>
-                {openBtn == true ? (
-                  <ProductDetail>
-                    · Italian fabric <br />
-                    · 100% Polyester <br />
-                    · Zip closure <br />
-                    · Storm flap with snap stud closure <br />
-                    · Belted <br />
-                    · Internal zip pocket <br />
-                    · Drawstring hood rolls away <br />
-                    · Two large flap pockets <br />
-                    · Relaxed fit <br />
-                    · M2201CBTE
-                    <br /> <br />
-                    Our Cape Parka in Black Tech. <br />
-                    Produced in an Italian technical fabric, our relaxed-fitting
-                    Cape Parka features features two large flap pockets, a storm
-                    flap with snap stud closure, and a drawstring hood that
-                    rolls away with snap fastening. <br /> <br />
-                    Our Cape Parka is true to size with a relaxed boxy fit. The
-                    model is 187 cm tall and is wearing a size 48. <br /> <br />
-                    All measurements are taken across the garment in cm while
-                    laid flat.
-                  </ProductDetail>
-                ) : undefined}
-              </div>
-              <ProductDetailblock>
-                <FirstDetailblock>SIZING</FirstDetailblock>
-                <SecondDetailblock>
-                  <InfoBtn
-                    onClick={() => {
-                      setOpenBtn((prevState) => !prevState);
-                      setRotateBtn((prevState) => !prevState);
-                    }}
-                  >
-                    {rotateBtn == true ? (
-                      <Rotation>
-                        <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
-                      </Rotation>
-                    ) : (
-                      <NonRotation>
-                        <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
-                      </NonRotation>
-                    )}
-                  </InfoBtn>
-                </SecondDetailblock>
-              </ProductDetailblock>
-
-              <ProductDetailblock>
-                <FirstDetailblock>SHIPPING</FirstDetailblock>
-                <SecondDetailblock>
-                  <InfoBtn
-                    onClick={() => {
-                      setOpenBtn((prevState) => !prevState);
-                      setRotateBtn((prevState) => !prevState);
-                    }}
-                  >
-                    {rotateBtn == true ? (
-                      <Rotation>
-                        <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
-                      </Rotation>
-                    ) : (
-                      <NonRotation>
-                        <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
-                      </NonRotation>
-                    )}
-                  </InfoBtn>
-                </SecondDetailblock>
-              </ProductDetailblock>
-            </div>
-          </ProductInfo>
+          <DetailBox infoname="Details"></DetailBox>
+          <DetailBox infoname="Size"></DetailBox>
+          <DetailBox infoname="SHipping"></DetailBox>
         </ContentDetail>
       </ContentContainer>
     </Container>
   );
 }
 
-const ProductInfo = styled.div`
-  float: left;
-  text-align: left;
-  margin-top: 60px;
-  font-size: 12px;
-  min-height: 48px;
-  font-family: 'Neue Haas Unica Pro', 'Helvetica', 'Arial', sans-serif;
-  font-weight: 700;
-  line-height: 1.3;
-  margin-bottom: -1px;
-  letter-spacing: 0.02em;
-  text-transform: uppercase;
-`;
-
-const ProductDetail = styled.p`
-  margin-top: 20px;
-  font-size: 0.8125rem;
-  font-family: 'Neue Haas Unica Pro', 'Helvetica', 'Arial', sans-serif;
-  font-weight: 400;
-  line-height: 1.4;
-  letter-spacing: 0em;
-`;
-
 const Container = styled.div`
-  margin-top: 180px;
+  margin-top: 190px;
+  @media (max-width: 1060px) {
+    justify-content: center;
+    display: flex;
+    margin-top: 160px;
+  }
 `;
 
 const ContentContainer = styled.section`
   width: 80%;
   display: inline-grid;
   grid-template-columns: repeat(2, 1fr);
+  @media (max-width: 1060px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
 `;
 
 const ContentImage = styled.div`
   flex: 1;
+  @media (max-width: 1060px) {
 `;
 
 const ContentDetail = styled.div`
   flex: 1;
   margin-left: 10px;
   padding-left: 40px;
+  @media (max-width: 1060px) {
+    padding-left:0;
+    margin-left:0;
 `;
 
 const Content = styled.div`
@@ -192,6 +119,9 @@ const Size = styled.div`
   margin-top: 20px;
   margin-bottom: 20px;
   padding-right: 24px;
+  @media (max-width: 1060px) {
+    width: 100%;
+  }
 `;
 
 const AddCart = styled.button`
@@ -204,48 +134,9 @@ const AddCart = styled.button`
   font-weight: 700;
   float: left;
   margin-top: 20px;
-`;
-
-const ProductDetailblock = styled.div`
-  display: flex;
-  height: 30px;
-  border-bottom: 1px solid;
-  width: 500px;
-  margin-bottom: 20px;
-`;
-
-const FirstDetailblock = styled.div`
-  flex: 1;
-`;
-
-const SecondDetailblock = styled.div`
-  flex: 1;
-  text-align: right;
-  justfy-content: right;
-  height: 30px;
-`;
-
-const InfoBtn = styled.button`
-  cursor: pointer;
-  background: white;
-  height: 25px;
-  border: none;
-`;
-
-const NonRotation = styled.svg`
-  width: 20px;
-  height: 30px;
-  cursor: pointer;
-  justify-content: center;
-`;
-
-const Rotation = styled.svg`
-  width: 20px;
-  height: 30px;
-  margin-bottom: 20px;
-  cursor: pointer;
-  justify-content: center;
-  transform: rotateX(180deg);
+  @media (max-width: 1060px) {
+    width: 100%;
+  }
 `;
 
 export default Detail;

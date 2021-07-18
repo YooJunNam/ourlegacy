193e97f478b4c4379e282178e783556aa764e9a1
YooJunNam <yjn7448@gmail.com>
2021-07-18 16:26:04 +0900

    Router Render 형식 변경

--- src/App.js
+++ src/App.js
@@ -4,36 +4,40 @@ import Header from './Header';
 import Body from './Body';
 import Footer from './Footer';
 import styled from 'styled-components';
-import Search from './modal/Search';
+import Search from './Navigation/Search';
 import { Link, Route, Switch } from 'react-router-dom';
-import Contact from './pages/Contact';
 import Category from './pages/Category';
 import Detail from './pages/Detail';
 import MobileCategory from './mobile/MobileCategory';
 import MobileBasketModal from './mobile/MobileBasketModal';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/account/Login';
+import Contact from './pages/Contact';
 
 function App() {
   const [searchClose, setSearchClose] = useState(true);
+  const [HamburgerModal, setHamburgerModal] = useState(false);
+  const [MobileBasketdata, SetMobileBasketdata] = useState(false);
+
   const openSearchModal = () => {
     setSearchClose(false);
   };
-  const [HamburgerModal, SetHamburgerModal] = useState(false);
-  const OpenHamburger = () => {
-    SetHamburgerModal((prevState) => !prevState);
+
+  const openHamburger = () => {
+    setHamburgerModal(true);
   };
 
-  const [MobileBasketdata, SetMobileBasketdata] = useState(false);
-
-  const handleOpenBasket = () => {
-    SetMobileBasketdata(true);
+  const closeHambugerModal = () => {
+    console.log(`closeHamburger`);
+    setHamburgerModal(false);
   };
+
   return (
     <div className="App">
       {MobileBasketdata == true ? <MobileBasketModal /> : undefined}
       <Header
         openSearchModal={openSearchModal}
-        OpenHamburger={OpenHamburger}
+        openHamburger={openHamburger}
       ></Header>
 
       <Route exact={true} path="/">
@@ -41,34 +45,56 @@ function App() {
           <MobileCategory
             HamburgerModal={HamburgerModal}
             SetMobileBasketdata={SetMobileBasketdata}
+            closeHamburgerModal={closeHambugerModal}
           ></MobileCategory>
         ) : (
           <>
             <Body></Body>
+            {/* <Login></Login> */}
           </>
         )}
       </Route>
 
-      <Route exact={true} path="/category">
-        {HamburgerModal == true ? (
-          <MobileCategory
-            HamburgerModal={HamburgerModal}
-            SetMobileBasketdata={SetMobileBasketdata}
-          ></MobileCategory>
-        ) : (
-          <>
-            <Category />
-          </>
-        )}
-      </Route>
+      <Route
+        exact={true}
+        path="/category/:categoryId"
+        render={(props) => {
+          return HamburgerModal == true ? (
+            <MobileCategory
+              {...props}
+              HamburgerModal={HamburgerModal}
+              SetMobileBasketdata={SetMobileBasketdata}
+              closeHamburgerModal={closeHambugerModal}
+            ></MobileCategory>
+          ) : (
+            <Category {...props} />
+          );
+        }}
+      ></Route>
 
-      <Route exact={true} path="/detail/:itemId" component={Detail}></Route>
+      <Route
+        exact={true}
+        path="/detail/:itemId"
+        render={(props) => {
+          return HamburgerModal == true ? (
+            <MobileCategory
+              {...props}
+              HamburgerModal={HamburgerModal}
+              SetMobileBasketdata={SetMobileBasketdata}
+              closeHamburgerModal={closeHambugerModal}
+            ></MobileCategory>
+          ) : (
+            <Detail {...props} />
+          );
+        }}
+      ></Route>
 
       <Route exact={true} path="/contact">
         {HamburgerModal == true ? (
           <MobileCategory
             HamburgerModal={HamburgerModal}
             SetMobileBasketdata={SetMobileBasketdata}
+            closeHamburgerModal={closeHambugerModal}
           ></MobileCategory>
         ) : (
           <>
@@ -76,6 +102,7 @@ function App() {
           </>
         )}
       </Route>
+
       {HamburgerModal == true ? undefined : (
         <>
           <Footer></Footer>
